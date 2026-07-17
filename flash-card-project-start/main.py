07ff43063f89589cca14520b07153f76057fa734
bi-gt-re-x <hanwenks@gmail.com>
2026-07-17 11:57:26 -0500

    Refactor flash card app to use pandas and improve logic

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,71 +1,149 @@
 from tkinter import *
-import pandas
+import pandas as pd
+import random
+from pathlib import Path
 
 BACKGROUND_COLOR = "#B1DDC6"
-data = pandas.read_csv(r"data\french_words.csv")
-french_words = data["French"].tolist()
-english_words = data["English"].tolist()
-random_num = 0
-french_word = french_words[random_num]
-english_word = english_words[random_num]
+FLIP_TIME = 3000
+
+DATA_FOLDER = Path("data")
+IMAGE_FOLDER = Path("images")
+
+try:
+    data = pd.read_csv(DATA_FOLDER / "words_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv(DATA_FOLDER / "french_words.csv")
+
+cards = data.to_dict(orient="records")
+
+current_card = {}
 flip_timer = None
 
-#Tkinter has a very 'clunky' interface. At this point, please learn HTML and CSS and build a website; this one works, so you can run it.
 
-#-----------------------------------------------------------------------------------------Card_Save-----------------------------------------------------------------------------------------#
-def memorized():
-    global random_num, english_word, french_word
-    english_words.remove(english_word)
-    french_words.remove(french_word)
+def save_progress():
+    remaining = pd.DataFrame(cards)
+    remaining.to_csv(DATA_FOLDER / "words_to_learn.csv", index=False)
 
-    new_word()
 
-#-----------------------------------------------------------------------------------------Word Bank-----------------------------------------------------------------------------------------#
-def new_word():
-    global french_word, random_num, flip_timer, english_word
+def choose_card():
+    global current_card
+    current_card = random.choice(cards)
+
+
+def show_front():
+    canvas.itemconfig(card_image, image=card_front)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word,
+                      text=current_card["French"],
+                      fill="black")
+
+
+def show_back():
+    canvas.itemconfig(card_image, image=card_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word,
+                      text=current_card["English"],
+                      fill="white")
+
+
+def next_card():
+    global flip_timer
+
+    if len(cards) == 0:
+        canvas.itemconfig(card_image, image=card_front)
+        canvas.itemconfig(card_title,
+                          text="Finished!",
+                          fill="black")
+        canvas.itemconfig(card_word,
+                          text="You've learned every word!",
+                          fill="black")
+        return
 
     if flip_timer is not None:
         window.after_cancel(flip_timer)
 
-    random_num += 1
-    french_word = french_words[random_num]
-    english_word = english_words[random_num]
+    choose_card()
+    show_front()
 
-    canvas.itemconfig(word, text=french_word)
-    canvas.itemconfig(title, text="French")
-    canvas.itemconfig(image, image=card_front)
+    flip_timer = window.after(FLIP_TIME, flip_card)
 
-    flip_timer = window.after(3000, flip_card)
 
-#------------------------------------------------------------------------------------------UI Setup-----------------------------------------------------------------------------------------#
+def flip_card():
+    show_back()
+
+
+def known_word():
+    if current_card in cards:
+        cards.remove(current_card)
+
+    save_progress()
+    next_card()
+
+
+def unknown_word():
+    next_card()
+
 window = Tk()
-window.title("Flash Card App")
+window.title("French Flash Cards")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.minsize(width=1000, height=800)
 
-card_back = PhotoImage(file=r"images\card_back.png")
-card_front = PhotoImage(file=r"images\card_front.png")
-canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-image = canvas.create_image(400, 263, image=card_front)
-title = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400, 263, text=french_word, font=("Ariel", 60, "bold"))
+card_front = PhotoImage(file=IMAGE_FOLDER / "card_front.png")
+card_back = PhotoImage(file=IMAGE_FOLDER / "card_back.png")
+
+canvas = Canvas(
+    width=800,
+    height=526,
+    bg=BACKGROUND_COLOR,
+    highlightthickness=0
+)
+
+card_image = canvas.create_image(
+    400,
+    263,
+    image=card_front
+)
+
+card_title = canvas.create_text(
+    400,
+    150,
+    text="French",
+    font=("Arial", 40, "italic"),
+    fill="black"
+)
+
+card_word = canvas.create_text(
+    400,
+    263,
+    text="",
+    font=("Arial", 60, "bold"),
+    fill="black"
+)
+
 canvas.pack()
 
-right_image = PhotoImage(file=r"images\right.png")
-right = Button(image=right_image, highlightthickness=0, command=memorized)
-right.place(x=575, y=560)
+wrong_image = PhotoImage(file=IMAGE_FOLDER / "wrong.png")
 
-wrong_image = PhotoImage(file=r"images\wrong.png")
-wrong = Button(image=wrong_image, highlightthickness=0, command=new_word)
-wrong.place(x=200, y=560)
+wrong_button = Button(
+    image=wrong_image,
+    borderwidth=0,
+    highlightthickness=0,
+    command=unknown_word
+)
 
-#------------------------------------------------------------------------------------------Card Flip----------------------------------------------------------------------------------------#
-def flip_card():
-    global english_word
-    
-    canvas.itemconfig(word, text=english_word)
-    canvas.itemconfig(title, text="English")
-    canvas.itemconfig(image, image=card_back)
+wrong_button.place(x=200, y=560)
+
+right_image = PhotoImage(file=IMAGE_FOLDER / "right.png")
+
+right_button = Button(
+    image=right_image,
+    borderwidth=0,
+    highlightthickness=0,
+    command=known_word
+)
+
+right_button.place(x=575, y=560)
+
+next_card()
 
 window.mainloop()
-
